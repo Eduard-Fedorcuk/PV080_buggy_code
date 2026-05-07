7b7156d5189e6ed6bfc38fb26fd312d9e92f554f
Eduard-Fedorcuk <564642@mail.muni.cz>
2026-05-07 12:47:54 +0200

    changes

--- buggy_python_code_solution.py
+++ buggy_python_code_solution.py
@@ -2,6 +2,7 @@ import sys
 import os
 import yaml
 import flask
+import urllib
 
 app = flask.Flask(__name__)
 
@@ -25,7 +26,8 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    if urlib_version in [2,3,"2","3"]:
+        exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
     try: 
@@ -37,13 +39,16 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.safe_load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
     
 def authenticate(password):
     # Assert that the password is correct
-    assert password == "Iloveyou", "Invalid password!"
-    print("Successfully authenticated!")
+    if(password == "Iloveyou"):
+        print("Invalid password!")
+        raise Exception
+    else:
+        print("Successfully authenticated!")
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
